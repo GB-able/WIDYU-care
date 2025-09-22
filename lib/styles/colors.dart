c19b1150d878b475f03821b9ed0296bafef3a64f
민감자 <mouse0429@naver.com>
2025-09-22 17:04:04 +0900

    fix: 컬러칩 변경

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -2,6 +2,9 @@ import 'dart:ui';
 
 class MyColor {
   static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+
+  static const grey50 = Color(0xFFFAFAFA);
   static const grey100 = Color(0xFFEEEEEE);
   static const grey200 = Color(0xFFE0E0E0);
   static const grey300 = Color(0xFFC9C9C9);
@@ -9,14 +12,55 @@ class MyColor {
   static const grey500 = Color(0xFF959595);
   static const grey600 = Color(0xFF636363);
   static const grey700 = Color(0xFF414141);
-  static const grey800 = Color(0xFF212121);
-  static const grey900 = Color(0xFF151515);
+  static const grey800 = Color(0xFF2D2D2D);
+  static const grey900 = Color(0xFF1F1F1F);
 
-  static const primary = Color(0xFFFF7618);
-  static const primaryLight = Color(0xFFFFF8EE);
-  static const secondary = Color(0xFF7200E4);
-  static const secondaryDark = Color(0xFF440484);
+  static const orange50 = Color(0xFFFFF4EE);
+  static const orange100 = Color(0xFFFFEEE4);
+  static const orange200 = Color(0xFFFFD1B6);
+  static const orange300 = Color(0xFFFFB082);
+  static const orange400 = Color(0xFFFF9E66);
+  static const orange500 = Color(0xFFFF8945);
+  static const orange600 = Color(0xFFFF7618);
+  static const orange700 = Color(0xFFD74F00);
+  static const orange800 = Color(0xFFAD4000);
+  static const orange900 = Color(0xFF863100);
 
-  static const red = Color(0xFFE52900);
-  static const redLight = Color(0xFFFFF2EF);
+  static const red50 = Color(0xFFFFF3F3);
+  static const red100 = Color(0xFFFFEBEB);
+  static const red200 = Color(0xFFFFCFCF);
+  static const red300 = Color(0xFFFF9999);
+  static const red400 = Color(0xFFFC7A7A);
+  static const red500 = Color(0xFFF45B5B);
+  static const red600 = Color(0xFFE41F10);
+  static const red700 = Color(0xFFCC1003);
+  static const red800 = Color(0xFF860C03);
+  static const red900 = Color(0xFF690700);
+
+  static const green50 = Color(0xFFF7FEF9);
+  static const green100 = Color(0xFFEEFFF3);
+  static const green200 = Color(0xFFD5F5DE);
+  static const green300 = Color(0xFFABECBD);
+  static const green400 = Color(0xFF85E7A1);
+  static const green500 = Color(0xFF55D97F);
+  static const green600 = Color(0xFF0BB53B);
+  static const green700 = Color(0xFF059C31);
+  static const green800 = Color(0xFF046F24);
+  static const green900 = Color(0xFF02571D);
+
+  static const purple50 = Color(0xFFFAF5FF);
+  static const purple100 = Color(0xFFEFDFFF);
+  static const purple200 = Color(0xFFDFC0FF);
+  static const purple300 = Color(0xFFCE9DFF);
+  static const purple400 = Color(0xFFB265FF);
+  static const purple500 = Color(0xFF912FF3);
+  static const purple600 = Color(0xFF7200E4);
+  static const purple700 = Color(0xFF5C00B9);
+  static const purple800 = Color(0xFF4C009A);
+  static const purple900 = Color(0xFF3D007A);
+
+  static const primary = orange500;
+  static const primaryLight = orange50;
+  static const secondary = purple600;
+  static const red = red600;
 }
