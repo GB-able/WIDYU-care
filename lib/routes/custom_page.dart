b2cde6b9571a66333374f1b7ffda130da7a840ff
민감자 <mouse0429@naver.com>
2025-12-29 19:21:22 +0900

    fix: close 애니메이션 라우트 타입 추가

--- lib/routes/custom_page.dart
+++ lib/routes/custom_page.dart
@@ -32,6 +32,34 @@ extension CustomGoRoute on GoRoute {
     );
   }
 
+  static GoRoute closeRoute({
+    required String path,
+    required Widget Function(BuildContext, GoRouterState) builder,
+    String? name,
+    Duration duration = duration,
+    List<RouteBase> routes = const [],
+  }) {
+    return GoRoute(
+      path: path,
+      name: name,
+      routes: routes,
+      pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: builder(context, state),
+        transitionDuration: duration,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: animation.drive(
+              Tween(begin: const Offset(0.0, 1.0), end: Offset.zero)
+                  .chain(CurveTween(curve: Curves.easeInOut)),
+            ),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
   static GoRoute fadeRoute({
     required String path,
     required Widget Function(BuildContext, GoRouterState) builder,
